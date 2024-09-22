7341183733a1b2f348fc8600da419ae6a45d4fbb
Mikalai Yakubau <mikalai.yakubau@ukg.com>
2024-09-22 17:09:39 +0300

    Init project

---
+++ my-angular-app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.less'
+})
+export class AppComponent {
+  title = 'my-angular-app';
+
+  setTitle(title: string) {
+    this.title = title;
+  }
+}
